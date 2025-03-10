9aacbbcb8a3a049a2a3145a93830af888e461546
Jacob Helzner <jhelzner@hamilton.edu>
2025-03-09 20:26:34 -0400

    commented script.js in the student interface

--- student-interface-demo/script.js
+++ student-interface-demo/script.js
@@ -1,54 +1,67 @@
+
+// Student class. Is able to plan courses and add them to their schedule
+
 class Student {
     constructor(start_term, track_or_major_list, contact_info, advisor_contact_info, completed_courses, planned_courses) {
-      this.start_term = start_term;
-      this.track_or_major_list = track_or_major_list;
-      this.contact_info = contact_info;
-      this.advisor_contact_info = advisor_contact_info;
-      this.completed_courses = completed_courses;
-      this.planned_courses = planned_courses;
+        this.start_term = start_term;
+        this.track_or_major_list = track_or_major_list;
+        this.contact_info = contact_info;
+        this.advisor_contact_info = advisor_contact_info;
+        this.completed_courses = completed_courses;
+        this.planned_courses = planned_courses;
     }
 
-    plan_course(target_course) {
-        this.planned_courses.push(target_course)
-        for (track_or_major of this.track_or_major_list) {
-            for (requirement of track_or_major.requirements) {
-                if (target_course.fulfills(requirement)) {
-                    requirement.fulfilled = true;
-                }
+    //add a given course to the schedule and fulfil the relevant requirements in
+    this students tracks / majors
+plan_course(target_course) {
+    this.planned_courses.push(target_course)
+    for (track_or_major of this.track_or_major_list) {
+        for (requirement of track_or_major.requirements) {
+            if (target_course.fulfills(requirement)) {
+                requirement.fulfilled = true;
             }
         }
-        target_course.number_of_students_who_planned += 1;
     }
+    target_course.number_of_students_who_planned += 1;
+}
 
-    can_plan_course(target_course) {
-        return this.has_prerequisites_for_course(target_course) && target_course.has_availability();
-    }
+//if the course is available and the student has the prerequisites,
+//it can be planned.
+can_plan_course(target_course) {
+    return this.has_prerequisites_for_course(target_course) && target_course.has_availability();
+}
 
-    has_prerequisites_for_course(target_course) {
-        number_unfulfilled = target_course.prerequisites.length;
-        for (course of this.completed_courses) {
-            if (course of target_course.prerequisites) {
-                number_unfulfilled -= 1;
-            }
+//check if there are enough completed courses that fulfil the prerequisite
+//(may need to redo this because its not technically how prerequisites work)
+has_prerequisites_for_course(target_course) {
+    number_unfulfilled = target_course.prerequisites.length;
+    for (course of this.completed_courses) {
+        if (course of target_course.prerequisites) {
+            number_unfulfilled -= 1;
         }
-        return (number_unfulfilled == 0);
     }
+    return (number_unfulfilled == 0);
+}
   }
 
-  class Course {
+//courses can be available and fill requirements, and also store other
+//relevant data like the term, prerequisites, capacity, name etc.
+class Course {
     constructor(name, weekly_schedule, term, prerequisites, capacity, number_of_students_who_planned) {
-      this.name = name;
-      this.weekly_schedule = weekly_schedule;
-      this.term = term;
-      this.prerequisites = prerequisites;
-      this.capacity = capacity;
-      this.number_of_students_who_planned = number_of_students_who_planned;
+        this.name = name;
+        this.weekly_schedule = weekly_schedule;
+        this.term = term;
+        this.prerequisites = prerequisites;
+        this.capacity = capacity;
+        this.number_of_students_who_planned = number_of_students_who_planned;
     }
 
+    //the course is available if the student count is strictly below capacity
     has_availability() {
         return (this.number_of_students_who_planned < this.capacity);
     }
 
+    //if this course is in the requirement, it fulfills it
     fulfills(requirement) {
         for (course of requirement.possible_courses_to_fulfill) {
             if (course.name == this.name) {
@@ -57,24 +70,28 @@ class Student {
         }
         return false
     }
-  }
+}
 
-  class Requirement {
+//a requirement may only be fulfilled by some courses, and has a term due date
+class Requirement {
     constructor(possible_courses_to_fulfill, term_due) {
         this.possible_courses_to_fulfill = possible_courses_to_fulfill;
         this.term_due = term_due;
         this.fulfilled = false;
-      }
-  }
-  //base class for tracks and majors
-  class TrackOrMajor {
+    }
+}
+
+//base class for tracks and majors, which are just named lists of requirements.
+class TrackOrMajor {
     constructor(name, requirements) {
         this.name = name;
         this.requirements = requirements;
     }
-  }
+}
 
-  class Database {
+//Interface with the remote database (in progress)
+
+class Database {
     build_list_of_all_courses() {
 
     }
@@ -86,20 +103,20 @@ class Student {
     build_student_object(student_identifier) {
 
     }
-  }
+}
 
 
+//test database that returns some dummy data
 
-
-  class TestDatabase extends Database {
+class TestDatabase extends Database {
 
 
     build_class_object(choice) {
-      if (choice == 1) {
-        const bio101 = new Course("BIO101", [["M", "1:00-1:50"],["F", "2:00-2:50"]], "FALL2025", [], 60, 12);
-          return bio101;
-      }
-      const chem201 = new Course("CHEM201", [["M", "1:00-1:50"],["W", "2:00-2:50"]], "SPRING2025", ["CHEM101"], 24, 11);
+        if (choice == 1) {
+            const bio101 = new Course("BIO101", [["M", "1:00-1:50"], ["F", "2:00-2:50"]], "FALL2025", [], 60, 12);
+            return bio101;
+        }
+        const chem201 = new Course("CHEM201", [["M", "1:00-1:50"], ["W", "2:00-2:50"]], "SPRING2025", ["CHEM101"], 24, 11);
         return chem201;
     }
 
@@ -110,6 +127,6 @@ class Student {
 
 
     build_list_of_all_courses() {
-        
+
     }
-  }
+}
